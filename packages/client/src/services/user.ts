5e8923761ba4f79260ddcb00ab314aea0400ad5d
Claiton Nazaret <claiton.nazaret@celk.net>
2023-02-21 15:26:22 -0300

    interceptores axios criados

--- packages/client/src/services/user.ts
+++ packages/client/src/services/user.ts
@@ -2,6 +2,6 @@ import Api from './api';
 
 export const UserService = {
     login: async (email: string, password: string) =>
-        await Api.post('/user/login', { email, password }),
+        await Api.post('/user/login', { email, password }, { withCredentials: true }),
     logout: async () => await Api.get('/user/logout'),
 };
