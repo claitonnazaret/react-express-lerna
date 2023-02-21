3255f1aea1c775504d9c343321533c56f939c7e4
Claiton Nazaret <claiton.nazaret@celk.net>
2023-02-21 16:46:49 -0300

    RegisterPage finalizado

--- packages/client/src/services/user.ts
+++ packages/client/src/services/user.ts
@@ -1,7 +1,11 @@
+import { IRegister } from '../types';
 import Api from './api';
 
+const baseUrl = '/user';
 export const UserService = {
     login: async (email: string, password: string) =>
-        await Api.post('/user/login', { email, password }, { withCredentials: true }),
+        await Api.post(`${baseUrl}/login`, { email, password }, { withCredentials: true }),
     logout: async () => await Api.get('/user/logout'),
+    register: async (data: IRegister) =>
+        await Api.post(`${baseUrl}/signup`, data, { withCredentials: true }),
 };
--- packages/client/src/types/index.ts
+++ packages/client/src/types/index.ts
@@ -1,6 +1,8 @@
 import { AxiosResponse } from 'axios';
 
+// Interfaces
 export interface IUser {
+    name?: string;
     email?: string;
     accessToken?: string;
 }
@@ -13,3 +15,19 @@ export interface IContext extends IUser {
 export interface IAuthProvider {
     children: JSX.Element;
 }
+
+export interface IResponse {
+    status: number;
+    message: string | null;
+    errors: string | null;
+    data: any | any[] | null;
+}
+
+// Types
+export type IRegister = {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    roleId: string;
+};
